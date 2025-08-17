af7992114d6ee9a22d1a47e5afbf84e0398e912f
Guillermo leira temes <104579306+Guille-ux@users.noreply.github.com>
2025-08-17 16:31:52 +0000

    Update eval.h

--- include/eval.h
+++ include/eval.h
@@ -80,6 +80,7 @@ Var *getShellVarEntry(EvalCtx *ctx, size_t id);
 void setShellVarEntry(EvalCtx *ctx, size_t id, ShellValue val);
 void exportShellVar(EvalCtx *ctx, size_t id);
 
+ShellValue evalExpr(ASTNode *expr, EvalCtx *ctx);
 int eval(ASTNode *ast, EvalCtx *ctx);
 
 #endif
